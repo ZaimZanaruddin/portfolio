38df04415f930c69bc608fb6ffd3f2070d43f767
ZaimZanaruddin <zaim.zanaruddin56@gmail.com>
2018-10-20 17:58:45 -0500

    Updates

---
+++ precache-manifest.b6c74e839ae8740669b1039a4e83f8e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "52a8de69421124c9f272",
+    "url": "/portfolio/static/js/main.52a8de69.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/portfolio/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "52a8de69421124c9f272",
+    "url": "/portfolio/static/css/main.7f4f6504.chunk.css"
+  },
+  {
+    "revision": "1db67369c9cb22a34fb7d719d93b56ee",
+    "url": "/portfolio/index.html"
+  }
+];
